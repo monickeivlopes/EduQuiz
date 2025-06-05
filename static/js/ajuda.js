f04cef5d92bf60a75356929dc9e957a3bb606513
Anna6864 <162826299+Anna6864@users.noreply.github.com>
2025-06-05 14:17:20 -0300

    Update ajuda.js

--- static/js/ajuda.js
+++ static/js/ajuda.js
@@ -1,19 +1,18 @@
+ const toggles = document.querySelectorAll('.faq-toggle');
+    const answers = document.querySelectorAll('.faq-answer');
+    const arrows = document.querySelectorAll('.arrow');
 
-        const toggles = document.querySelectorAll('.faq-toggle');
-        const answers = document.querySelectorAll('.faq-answer');
-        const arrows = document.querySelectorAll('.arrow');
-    
-        toggles.forEach((btn, index) => {
-            btn.addEventListener('click', () => {
-                answers.forEach((answer, i) => {
-                    if (i === index) {
-                        const isActive = answer.classList.contains('active');
-                        answer.classList.toggle('active', !isActive);
-                        arrows[i].classList.toggle('rotate', !isActive);
-                    } else {
-                        answer.classList.remove('active');
-                        arrows[i].classList.remove('rotate');
-                    }
-                });
-            });
+    toggles.forEach((btn, index) => {
+      btn.addEventListener('click', () => {
+        answers.forEach((answer, i) => {
+          if (i === index) {
+            const isActive = answer.classList.contains('active');
+            answer.classList.toggle('active', !isActive);
+            arrows[i].classList.toggle('rotate', !isActive);
+          } else {
+            answer.classList.remove('active');
+            arrows[i].classList.remove('rotate');
+          }
         });
+      });
+    });
